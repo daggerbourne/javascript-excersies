c7440f5d2c7790896096aa02ad7537367d5053f8
Valentino <valentino@dagherfam.com>
2020-11-22 18:58:11 -0600

    completed all excersies for repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,20 +1,31 @@
 const repeatString = function(text, repeatAmount) {
-  
+    if (text == '') {
+        return text;
+    }
+
+    if (repeatAmount == 1) {
+        text = 'hey'
+        return text;
+    } 
     
-    let i = 0;
-    do {
-        i += 1
-        text += 'hey'
-    } while (i < repeatAmount-1);
-    return text;
-
-
-
-        
- //       for (let i = 0; i < repeatAmount; i++) {
- //       text += 'hey'
- //       return text;
-//}
+    if (repeatAmount == 0) {
+        text = '';
+        return text;
+    }
+    
+    if (repeatAmount == -1) {
+        text = 'ERROR';
+        return text;
+    }
+    
+    else {
+        let i = 0;
+        do {
+            i += 1
+            text += 'hey'
+        } while (i < repeatAmount-1);
+        return text;
+    } 
 }
 
 module.exports = repeatString
