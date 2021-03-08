f99eeffe9e5e35e9438251afa3ae369143901c50
daggerbourne <valentino@dagherfam.com>
2021-03-08 09:53:46 -0600

    completed calc, started palen

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
+const palindromes = function(str) {
+let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+let rawChar = str;
+let cleanChar = rawChar.filter(function(letter){
+    return punctuation.indexOf(letter) === -1;
+})
+let cleanString = cleanChar.join(''); 
+let reverStr = cleanString.split('').reverse().join('');
+str.toLowerCase();
 
+return reverStr === str;
 }
 
-module.exports = palindromes
+// module.exports = palindromes
